555ee58de7e9784fb8b2f4fec31a88890857fecf
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-11-24 23:13:53 +0000

    corrected some of the code

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 #Using Puppet, create a manifest that kills a process named killmenow
 exec {'kill':
-commad   => 'pkill -f killmenow',
-provider => shell,
+  command   => 'pkill -f killmenow',
+  path     => '/bin:/usr/bin',
+  user     => 'root',
+  onlyif   => 'pgrep -f killmenow',
 }
