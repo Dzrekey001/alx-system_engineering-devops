675dad7abf0262895d29a906b40a9c62a7c00ba9
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-11-25 09:19:34 +0000

    edited code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # Using Puppet, install flask from pip3
 package { 'flask':
-  ensure   => 'latest',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
