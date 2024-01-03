57d317550c64170ac747a23b113f8781df74b48f
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-01-03 01:18:59 +0000

    Initial commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees"""
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    users = requests.get(url + "/users/?id={}".format(user_id))
+    todos = requests.get(url + "/todos?userId={}".format(user_id))
+
+    TASK_DONE = [n for n in todos.json() if n['completed']]
+    NUMBER_OF_DONE_TASKS = len(TASK_DONE)
+    TOTAL_NUMBER_OF_TASKS = len(todos.json())
+    USERNAME = users.json()[0].get('name')
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(USERNAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for task in TASK_DONE:
+        print("\t {}".format(task.get("title")))
+
+    rows = [[user_id, USERNAME, n["completed"],
+            n["title"]] for n in todos.json()]
+
+    with open("USER_ID.csv", mode="w") as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME",
+                        "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        writer.writerows(rows)
