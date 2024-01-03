524af8a4264263d7bc1bb960d2afc3e0c5d06c8c
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-01-03 02:05:41 +0000

    edited code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,14 +9,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     users = requests.get(url + "/users/?id={}".format(user_id))
     todos = requests.get(url + "/todos?userId={}".format(user_id))
-
-    TASK_DONE = [n for n in todos.json() if n['completed']]
-    NUMBER_OF_DONE_TASKS = len(TASK_DONE)
-    TOTAL_NUMBER_OF_TASKS = len(todos.json())
-    USERNAME = users.json()[0].get('name')
-
-    rows = [[user_id, USERNAME, n.get("completed"),
-            n.get("title")] for n in todos.json()]
+    USERNAME = users.json()[0].get('username')
 
     with open("{}.csv".format(user_id), mode="w") as file:
         for task in todos.json():
