28375fc7ce431446ef5de7d58c77904c40728b6b
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-01-03 02:33:35 +0000

    Initial commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    users = requests.get(url + "/users/?id={}".format(user_id))
+    todos = requests.get(url + "/todos?userId={}".format(user_id))
+    USERNAME = users.json()[0].get('username')
+    data = [{"task": t.get("title"), "completed": t.get("completed"),
+            "username": USERNAME} for t in todos.json()]
+
+    with open("{}.json".format(user_id), mode="w") as file:
+        json.dump({user_id: data}, file)
